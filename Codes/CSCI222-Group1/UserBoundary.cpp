dab84276336cc842704f3fce2a5729c7625fc7e4
swn881 <swn881@uowmail.edu.au>
2014-09-25 22:32:49 +1000

    changes made

--- Codes/CSCI222-Group1/UserBoundary.cpp
+++ Codes/CSCI222-Group1/UserBoundary.cpp
@@ -5,5 +5,27 @@ using namespace std;
 
 void UserBoundary::registerUser()
 {
+    string tempUN;
+    string tempFN;
+    string tempLN;
+    string tempEmail;
+    string tempUni;
+    string tempExp;
+    string tempPw;
     cout << "Please input as stated..." << endl << endl;
+
+    cout << "User name: ";
+    cin >> tempUN;
+    cout << "First name: ";
+    cin >> tempFN;
+    cout << "Last name: ";
+    cin >> tempLN;
+    cout << "Email: ";
+    cin >> tempEmail;
+    cout << "University: ";
+    cin >> tempUni;
+    cout << "Expertise: ";
+    cin >> tempExp;
+    cout << "Password: ";
+    cin >> tempPw;
 }
