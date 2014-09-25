8e5360955d82148014475b7389690560906d4bc2
swn881 <swn881@uowmail.edu.au>
2014-09-25 21:47:51 +1000

    Implementation begins

---
+++ Codes/CSCI222-Group1/UserBoundary.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "UserBoundary.h"
+
+using namespace std;
+
+void UserBoundary::registerUser()
+{
+
+}
---
+++ Codes/CSCI222-Group1/UserBoundary.h
@@ -0,0 +1,14 @@
+#include <iostream>
+
+#ifndef _USERBOUNDARY_
+#define _USERBOUNDARY_
+
+class UserBoundary
+{
+    private:
+
+    public:
+        void registerUser();
+};
+
+#endif // _USERBOUNDARY_
