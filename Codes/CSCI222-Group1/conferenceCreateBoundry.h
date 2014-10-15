2946709f77d40c3b7a9b277d689c39c0c8c6de1f
drw963 <drw963@users.noreply.github.com>
2014-10-15 15:09:08 +1100

    adding paper submit and create conference

---
+++ Codes/CSCI222-Group1/conferenceCreateBoundry.h
@@ -0,0 +1,13 @@
+#ifndef CONFERENCECREATEBOUNDRY_H_INCLUDED
+#define CONFERENCECREATEBOUNDRY_H_INCLUDED
+
+
+class conferenceCreate
+{
+public:
+    conferenceCreate(){};
+    ~conferenceCreate(){};
+    int create();
+};
+
+#endif // CONFERENCECREATEBOUNDRY_H_INCLUDED
