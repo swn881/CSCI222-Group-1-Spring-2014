b9d02775a3b1a2d97e4e1c369277ac8ab9dbeb72
drw963 <drw963@users.noreply.github.com>
2014-10-15 15:04:40 +1100

    adding paper submit and create conference

---
+++ Codes/CSCI222-Group1/paperSubmitBoundry.h
@@ -0,0 +1,13 @@
+#ifndef PAPERSUBMITBOUNDRY_H_INCLUDED
+#define PAPERSUBMITBOUNDRY_H_INCLUDED
+
+class paperView
+{
+public:
+	paperView(){};
+	~paperView(){};
+    void submit();
+
+};
+
+#endif // PAPERSUBMITBOUNDRY_H_INCLUDED
